50994f13219eed55f8e677a61b450578591c6532
DavidNNussbaum <brocknynj@aol.com>
2021-05-26 20:10:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+class Polygon(arr) {
+    get countSides {
+        return arr.length
+    }
+
+    get perimeter {
+        arr.reduce(function(total, num){
+            return num + total        
+    }
+   }
+  
+}
+
+class Triangle extends Polygon {
+    get isValid {
+        if {arr[0] + arr[1] < arr[2]} || {arr[1] + arr[2] < arr[0]} || {arr[0] + arr[2] < arr[1]}
+        true
+    } else {
+        false
+    }
+    }
+}
